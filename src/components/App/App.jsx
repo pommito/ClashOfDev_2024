67c241458c400be06234c2fd5c43a4165f0873b5
Victor Lebecq <victor.lebecq.pro@gmail.com>
2024-09-21 14:16:26 +0200

    Header is now in position fixed

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,10 @@
 import Header from '../layouts/Header/Header';
-import Cursor from '../utils/Cursor/Cursor';
 import './App.scss';
 
 function App() {
   return (
     <>
       <Header />
-      <Cursor color="green" pseudo="@maislina_" isReversed={false} />
     </>
   );
 }
