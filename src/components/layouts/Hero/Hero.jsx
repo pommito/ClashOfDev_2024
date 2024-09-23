e11b5f566c5cfabafff0af5b2c7a6a0da940a82e
Victor Lebecq <victor.lebecq.pro@gmail.com>
2024-09-23 20:43:31 +0200

    Don't have the time to handle motion and easter egg, feels bad man

--- src/components/layouts/Hero/Hero.jsx
+++ src/components/layouts/Hero/Hero.jsx
@@ -6,30 +6,6 @@ import logoCOD from '../../../assets/heroLogoCOD.svg';
 import './Hero.scss';
 
 const Hero = () => {
-  const cursorsRef = useRef([]);
-
-  useEffect(() => {
-    const animateCursors = () => {
-      cursorsRef.current.forEach((cursor, index) => {
-        console.log(index);
-
-        if (cursor) {
-          // Vérification de sécurité
-          const speed = 0.5 + index * 0.1;
-          const angle = Date.now() * 0.001 * speed;
-          const x = Math.sin(angle) * 20;
-          const y = Math.cos(angle) * 20;
-
-          cursor.style.transform = `translate(-${x}px, ${y}px)`;
-        }
-      });
-
-      requestAnimationFrame(animateCursors);
-    };
-
-    animateCursors();
-  }, []);
-
   return (
     <section className="Hero">
       <div className="Hero-content">
@@ -43,9 +19,9 @@ const Hero = () => {
         <div className="Hero-content-visual">
           <img src={card} alt="the card of clash of devs" className="Hero-content-visual-card" />
           <img src={logoCOD} alt="Logo clash of dev" className="Hero-content-visual-logo" />
-          <Cursor ref={(el) => (cursorsRef.current[0] = el)} color="pink" pseudo="@sometimecrea" isReversed={true} />
-          <Cursor ref={(el) => (cursorsRef.current[1] = el)} color="orange" pseudo="@maislina_" isReversed={false} />
-          <Cursor ref={(el) => (cursorsRef.current[2] = el)} color="blue" pseudo="@LLCoolChris" isReversed={false} />
+          <Cursor color="pink" pseudo="@sometimecrea" isReversed={true} />
+          <Cursor color="orange" pseudo="@maislina_" isReversed={false} />
+          <Cursor color="blue" pseudo="@LLCoolChris" isReversed={false} />
         </div>
       </div>
       <h1 className="Hero-title">&lt;A&gt; Front les tous ! Clash of Devs !</h1>
--- src/components/utils/Cursor/Cursor.jsx
+++ src/components/utils/Cursor/Cursor.jsx
@@ -1,15 +1,14 @@
-import { forwardRef } from 'react';
 import './Cursor.scss';
 
-const Cursor = forwardRef(({ color, pseudo, isReversed }, ref) => {
+const Cursor = ({ color, pseudo, isReversed }) => {
   return (
-    <div ref={ref} className={`Cursor ${color} ${isReversed ? 'Reversed' : ''}`}>
+    <div className={`Cursor ${color} ${isReversed ? 'Reversed' : ''}`}>
       <svg className={color} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
         <path d="M5.5 3.21V20.8c0 .45.54.67.85.35l4.86-4.86a.5.5 0 0 1 .35-.15h6.87a.5.5 0 0 0 .35-.85L6.35 2.85a.5.5 0 0 0-.85.35Z"></path>
       </svg>
       <span className={`Cursor-text ${color}`}>{pseudo}</span>
     </div>
   );
-});
+};
 
 export default Cursor;
